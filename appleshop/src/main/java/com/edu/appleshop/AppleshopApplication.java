8a9704e9017564ca17289a438664b77c1cea8211
quyen2k5 <nguyenducquyen25022005@gmail.com>
2025-03-17 10:21:49 +0700

    init project

---
+++ appleshop/src/main/java/com/edu/appleshop/AppleshopApplication.java
@@ -0,0 +1,13 @@
+package com.edu.appleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleshopApplication.class, args);
+	}
+
+}
---
+++ appleshop/src/test/java/com/edu/appleshop/AppleshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.appleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
