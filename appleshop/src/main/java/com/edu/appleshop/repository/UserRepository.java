e0bbb5f909dc883170f7083f14b944c537d2a03f
quyen2k5 <nguyenducquyen25022005@gmail.com>
2025-03-24 02:05:38 +0700

    Update UserRepository.java

--- appleshop/src/main/java/com/edu/appleshop/repository/UserRepository.java
+++ appleshop/src/main/java/com/edu/appleshop/repository/UserRepository.java
@@ -1,9 +1,9 @@
-package com.exe.shojin.repository;
+package com.edu.appleshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.exe.shojin.model.User;
+import com.edu.appleshop.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
