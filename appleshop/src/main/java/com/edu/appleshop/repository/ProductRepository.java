9c3207c55d855b7b137e0054972be5e131f89529
quyen2k5 <nguyenducquyen25022005@gmail.com>
2025-03-23 22:36:17 +0700

    Update ProductRepository.java

--- appleshop/src/main/java/com/edu/appleshop/repository/ProductRepository.java
+++ appleshop/src/main/java/com/edu/appleshop/repository/ProductRepository.java
@@ -2,7 +2,7 @@ package com.edu.appleshop.repository;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.exe.shojin.model.Product;
+import com.edu.appleshop.model.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
 
