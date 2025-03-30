0be73d233f3846cc4f9ea84f6694ad43bff85aaa
quyen2k5 <nguyenducquyen25022005@gmail.com>
2025-03-30 19:59:20 +0700

    Update ProductService.java

--- appleshop/src/main/java/com/edu/appleshop/service/ProductService.java
+++ appleshop/src/main/java/com/edu/appleshop/service/ProductService.java
@@ -1,10 +1,19 @@
 package com.edu.appleshop.service;
 
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.edu.appleshop.model.Product;
 
 public interface ProductService {
 
     Iterable<Product> findAll();
+    void add(Product product);
     List<Product> getProductsByLoai(String category);
-
+     Product findByID(String productID);
+     void delete(String productID);
+     Page<Product> searchName(String searchNameProduct, Pageable pageable);
 }
