3bce4220f5265af7bd41a24c35c76acc33253f75
quyen2k5 <nguyenducquyen25022005@gmail.com>
2025-03-30 20:00:27 +0700

    Update UserService.java

--- appleshop/src/main/java/com/edu/appleshop/service/UserService.java
+++ appleshop/src/main/java/com/edu/appleshop/service/UserService.java
@@ -1,7 +1,9 @@
 package com.edu.appleshop.service;
 import com.edu.appleshop.model.User;
 public interface UserService {
+    Iterable<User> findAll();
     void add(User user);
     boolean existsByUsername(String username);
     User findById(String username);
+    void delete(String username);
 }
