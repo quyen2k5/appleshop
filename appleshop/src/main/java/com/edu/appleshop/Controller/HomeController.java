e56be1bb9e8dd480598e975f2f0e138ae036cc88
quyen2k5 <nguyenducquyen25022005@gmail.com>
2025-03-30 19:41:27 +0700

    Update HomeController.java

--- appleshop/src/main/java/com/edu/appleshop/Controller/HomeController.java
+++ appleshop/src/main/java/com/edu/appleshop/Controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.edu.appleshop.Controller;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 import com.exe.shojin.model.Product;
 import com.exe.shojin.model.User;
@@ -21,37 +23,69 @@ public class HomeController {
 
     @Autowired
     private ProductService productService;
+
     @GetMapping("/home")
     public String showHome(Model model) {
         List<Product> sanPhamIphone = productService.getProductsByLoai("iphone");
         List<Product> sanPhamMacBook = productService.getProductsByLoai("macbook");
+        List<Product> sanPhamAirPod = productService.getProductsByLoai("airpods");
+        List<Product> sanPhamAppleWatch = productService.getProductsByLoai("applewatch");
         var product = productService.findAll();
         model.addAttribute("products", product);
         model.addAttribute("sanPhamIphone", sanPhamIphone);
         model.addAttribute("sanPhamMacBook", sanPhamMacBook);
+        model.addAttribute("sanPhamAirPod", sanPhamAirPod);
+        model.addAttribute("sanPhamAppleWatch", sanPhamAppleWatch);
 
-        
-        return "home";  
-    } 
-    @GetMapping("/user")
-    public String productHome( Model model,HttpSession session) {
+        return "home";
+    }
+
+    @GetMapping("/home2")
+    public String showHome2(Model model, HttpSession session) {
+        List<Product> sanPhamIphone = productService.getProductsByLoai("iphone");
+        List<Product> sanPhamMacBook = productService.getProductsByLoai("macbook");
+        List<Product> sanPhamAirPod = productService.getProductsByLoai("airpods");
+        List<Product> sanPhamAppleWatch = productService.getProductsByLoai("applewatch");
+        var product = productService.findAll();
         User logUser = (User) session.getAttribute("logUser");
         if (logUser != null) {
             model.addAttribute("user", logUser);
-            return "redirect:/index/home"; 
         }
-        return  "redirect:/login"; 
+        model.addAttribute("products", product);
+        model.addAttribute("sanPhamIphone", sanPhamIphone);
+        model.addAttribute("sanPhamMacBook", sanPhamMacBook);
+        model.addAttribute("sanPhamAirPod", sanPhamAirPod);
+        model.addAttribute("sanPhamAppleWatch", sanPhamAppleWatch);
+        
+        return "home2";
     }
-     @GetMapping("/admin")
+
+    @GetMapping("/user")
+    public String productHome(Model model, HttpSession session) {
+        User logUser = (User) session.getAttribute("logUser");
+        if (logUser != null) {
+            model.addAttribute("user", logUser);
+            return "redirect:/index/home2";
+
+        }
+        return "redirect:/login";
+    }
+
+    @GetMapping("/admin")
     public String adminHome(Model model, HttpSession session) {
         User logUser = (User) session.getAttribute("logUser");
         if (logUser != null) {
             model.addAttribute("user", logUser);
-            return "admin";
+            return "redirect:/admin/home";
         }
-        return "redirect:/login";
+        return "redirect:/login/log";
     }
+
    
 
+
+
+
 }
+
 
