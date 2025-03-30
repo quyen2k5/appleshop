a381eba9c0c650bca00d1e1b3e4ee8e37fa3b569
quyen2k5 <nguyenducquyen25022005@gmail.com>
2025-03-30 19:53:10 +0700

    Update ProductServiceImpl.java

--- appleshop/src/main/java/com/edu/appleshop/service/impl/ProductServiceImpl.java
+++ appleshop/src/main/java/com/edu/appleshop/service/impl/ProductServiceImpl.java
@@ -1,6 +1,11 @@
 package com.edu.appleshop.service.impl;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.edu.appleshop.model.Product;
@@ -14,12 +19,28 @@ public class ProductServiceImpl implements ProductService {
     public Iterable<Product> findAll() {
         return productRepository.findAll();
     }
+    public void add(Product product) {
+        productRepository.save(product);
+    }
+
+    public void delete(String productID) {
+        productRepository.deleteById(productID);
+    }
+
     public List<Product> getProductsByLoai(String category) {
         return productRepository.findByCategory(category);
     }
-    
 
+    public Product findByID(String productID){
+        Optional<Product> product = productRepository.findById(productID);
+        return product.orElse(null);
+    }   
+
+    public Page<Product> searchName(String searchNameProduct, Pageable pageable) {
+        return productRepository.search(searchNameProduct, pageable);
+    }
     
 
 
 }
+
