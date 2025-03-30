b0558db38b89ad7ddc70451f400125181c87da69
quyen2k5 <nguyenducquyen25022005@gmail.com>
2025-03-30 20:12:52 +0700

    Update UserController.java

--- appleshop/src/main/java/com/edu/appleshop/Controller/UserController.java
+++ appleshop/src/main/java/com/edu/appleshop/Controller/UserController.java
@@ -1,4 +1,4 @@
-package com.exe.shojin.Controller;
+package com.edu.appleshop.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
