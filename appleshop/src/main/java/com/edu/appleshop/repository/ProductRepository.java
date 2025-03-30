198c9d32bd75c094a969ade57b0e965cf3c2a0d4
quyen2k5 <nguyenducquyen25022005@gmail.com>
2025-03-30 19:48:07 +0700

    Update ProductRepository.java

--- appleshop/src/main/java/com/edu/appleshop/repository/ProductRepository.java
+++ appleshop/src/main/java/com/edu/appleshop/repository/ProductRepository.java
@@ -1,13 +1,24 @@
 package com.edu.appleshop.repository;
 
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.edu.appleshop.model.Product;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
 
     List<Product> findByCategory(String category);
 
+    Page<Product> findByProductNameContaining(String productName, Pageable pageable);
 
+    @Query("SELECT p FROM Product p WHERE p.productName LIKE ?1")
+    Page<Product> search(String searchNameProduct, Pageable pageable);
 
 }
