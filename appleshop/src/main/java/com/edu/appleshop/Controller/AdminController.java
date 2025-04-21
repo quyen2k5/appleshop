0f6f40b92e42c0e15eebed99ea142d2621471257
DinhHuynhDuc <dinhduc9977@gmail.com>
2025-04-21 10:52:30 +0700

    Update AdminController.java

--- appleshop/src/main/java/com/edu/appleshop/Controller/AdminController.java
+++ appleshop/src/main/java/com/edu/appleshop/Controller/AdminController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
 
 import com.edu.appleshop.model.Product;
 import com.edu.appleshop.model.User;
